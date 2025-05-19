a2b81bb0f536bdba9157316b26d85b9b9b76fe2c
yepcog <yep0cog41@gmail.com>
2025-05-19 22:13:07 +0300

    Setting up a C++ Development Environment

---
+++ 2025-05-19_Introduction to C++/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+int main() {
+// 1: Setting up a C++ Development Environment
+
+	//Hello World!
+	std::cout << "Hello World!";
+
+	//Hello World!My name is CogGoodbye
+	std::cout << "Hello World!";
+	std::cout << "My name is Cog";
+	std::cout << "Goodbye";
+	
+	//Hello World!
+	//My name is Cog
+	//Goodbye
+	std::cout << "\n";
+	std::cout << "Hello World!";
+	std::cout << "\n";
+	std::cout << "My name is Cog";
+	std::cout << "\n";
+	std::cout << "Goodbye";
+	std::cout << "\n";
+	
+	//Hello World!
+	//My name is Cog
+	//Goodbye
+	std::cout << "Hello World!\n";
+	std::cout << "My name is Cog\nGoodbye\n";
+
+	//Hello World!
+	//My name is Cog
+	//Goodbye
+	std::cout << "Hello World!";
+	std::cout << std::endl;
+	std::cout << "My name is Cog" << std::endl;
+	std::cout << "Goodbye" << "\n";
+
+	//Hello World!
+	//My name is Cog
+	//Goodbye
+	std::cout << "Hello World!\n"
+		<< "My name is Cog\n"
+		<< "Goodbye" << "\n";
+
+	//14 is a number
+	//Another number is 41
+	std::cout << 14 << " is a number\n"
+		<< "Another number is " << 41 << "\n";
+
+	return 0;
+}
